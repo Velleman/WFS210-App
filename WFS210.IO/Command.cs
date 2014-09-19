f943e3b6c57b32aa04d2b83b6cc5144fb3d696c4
velbn <bn@velleman.be>
2014-09-19 14:16:48 +0200

    Chang static readonly to const cause velgvb is nub

--- WFS210.IO/Command.cs
+++ WFS210.IO/Command.cs
@@ -10,43 +10,43 @@ namespace WFS210.IO
 		/// <summary>
 		/// Request WiFi settings.
 		/// </summary>
-		public static readonly byte RequestWifiSettings = 0x0a;
+		public const byte RequestWifiSettings = 0x0a;
 
 		/// <summary>
 		/// Wifi settings.
 		/// </summary>
-		public static readonly byte WifiSettings = 0x22;
+		public const byte WifiSettings = 0x22;
 
 		/// <summary>
 		/// Request device settings (Timebase etc).
 		/// </summary>
-		public static readonly byte RequestSettings = 0x10;
+		public const byte RequestSettings = 0x10;
 
 		/// <summary>
 		/// Device settings.
 		/// </summary>
-		public static readonly byte Settings = 0x20;
+		public const byte Settings = 0x20;
 
 		/// <summary>
 		/// Modify device settings.
 		/// </summary>
-		public static readonly byte ModifySettings = 0x0b;
+		public const byte ModifySettings = 0x0b;
 
 		/// <summary>
 		/// Start the calibration process.
 		/// </summary>
-		public static readonly byte StartCalibration = 0xca;
+		public const byte StartCalibration = 0xca;
 
 		/// <summary>
 		/// Request sample data. Samples are usually sent by the device
 		/// automatically, so there is not much need to use this command.
 		/// </summary>
-		public static readonly byte RequestSampleData = 0x12;
+		public const byte RequestSampleData = 0x12;
 
 		/// <summary>
 		/// Sample data.
 		/// </summary>
-		public static readonly byte SampleData = 0x21;
+		public const byte SampleData = 0x21;
 	}
 }
 
