88fdc42c4ca54f8fb4aecbd179589da008c6b3b9
velbn <bn@velleman.be>
2014-09-25 15:36:45 +0200

    switch from live to demo and back to live is fixed

--- WFS210.UI/SettingsViewController.cs
+++ WFS210.UI/SettingsViewController.cs
@@ -9,8 +9,11 @@ namespace WFS210.UI
 	partial class SettingsViewController : UIViewController
 	{
 		public event EventHandler<EventArgs> RequestedDismiss;
-		public WifiSettings WifiSetting{ get; set;}
-		public ServiceManager ServiceManager{ get; set;}
+
+		public WifiSettings WifiSetting{ get; set; }
+
+		public ServiceManager ServiceManager{ get; set; }
+
 		public SettingsViewController (IntPtr handle) : base (handle)
 		{
 		}
@@ -19,30 +22,29 @@ namespace WFS210.UI
 		{
 			base.ViewDidLoad ();
 			btnDismiss.TouchUpInside += (object sender, EventArgs e) => {
-				WifiSetting.Channel = int.Parse(txtWifiChannel.Text);
+				WifiSetting.Channel = int.Parse (txtWifiChannel.Text);
 				WifiSetting.Name = txtWifiName.Text;
 				WifiSetting.Password = txtWifiPassword.Text;
-				if(RequestedDismiss !=null)
-					RequestedDismiss(this,null);
+				if (RequestedDismiss != null)
+					RequestedDismiss (this, null);
 			};
 
 			swDemo.ValueChanged += (object sender, EventArgs e) => {
-				if(swDemo.On)
-				{
-					NSUserDefaults.StandardUserDefaults.SetBool(true,"demo");
+				if (swDemo.On) {
+					NSUserDefaults.StandardUserDefaults.SetBool (true, "demo");
 					ServiceManager.ServiceType = ServiceType.Demo;
-				}
-				else
-				{
-					NSUserDefaults.StandardUserDefaults.SetBool(false,"demo");
+				} else {
+					NSUserDefaults.StandardUserDefaults.SetBool (false, "demo");
 					ServiceManager.ServiceType = ServiceType.Live;
-					ServiceManager.ActiveService.RequestSettings();
+					if (!ServiceManager.ActiveService.Active)
+						ServiceManager.ActiveService.Activate ();
+					ServiceManager.ActiveService.RequestSettings ();
 					ServiceManager.ActiveService.RequestSamples ();
 				}
 			};
 
 			txtWifiName.Text = WifiSetting.Name;
-			txtWifiChannel.Text = WifiSetting.Channel.ToString();
+			txtWifiChannel.Text = WifiSetting.Channel.ToString ();
 			if (ServiceManager.ServiceType == ServiceType.Demo)
 				swDemo.On = true;
 			else
